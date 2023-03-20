cddcd101d2b3d6c33cc7c4f245a20eec0e7a8b35
Sharizz <sharizz@Sharizzs-MacBook-Pro.local>
2023-03-21 01:36:12 +0530

    Laying the foundation

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-const heading1 = React.createElement("h1", {}, "Namaste React!");
-const heading2 = React.createElement("h2", {}, "Namaste Javascript!");
-const container = React.createElement("div", { classname: "conatiner" }, [heading1, heading2]);
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+// React.createElement => Object => HTML(DOM)
+
+const heading = (
+    <h3>Namste React!</h3>
+); //React Element
+
+const Title = () => (
+    <h3>Namste React Title!</h3>
+); //React Element
+
+var xyz = 10;
+
+const HeaderComponent = () => {
+    return (
+        <div> 
+            {xyz}
+            <h1>Namaste React functional component</h1>
+            <h2>This is a h2 tag</h2>
+            {heading}
+            <Title/>
+            {Title()}
+        </div>
+    )
+}// React Component
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container)
+root.render(<HeaderComponent />)
